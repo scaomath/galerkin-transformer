749d1db29674b06911c00f503c3725a0ac4682f8
S.Cao <scao.math@gmail.com>
2021-05-07 11:09:57 -0500

    update requirements and Burgers example

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # Fourier Transformer and Galerkin Transformer: Attention without softmax
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](./LICENSE)
+[![Python 3.7](https://img.shields.io/badge/python-3.7-blue.svg)](https://www.python.org/downloads/release/python-370/)
+[![Pytorch 1.8](https://img.shields.io/badge/pytorch-1.8-blue.svg)](https://pytorch.org/)
+
 This is the repository for paper:
 
 # Data
 The data is of courtesy of [Mr. Zongyi Li (Caltech)](https://github.com/zongyi-li): https://github.com/zongyi-li/fourier_neural_operator
 
 # Examples
+All examples are learning PDE-related operators. The setting can be found in [`config.yml`](./config.yml)
 
 ## Burgers
+The baseline benchmark [`ex1_burgers.py`](./ex1_burgers.py) is using `1cycle` for 100 epochs, 2 FT layers as encoder and 2 SpectralConv1d layers from [Li et al 2020](https://github.com/zongyi-li/fourier_neural_operator) as decoder. Inference relative error is about `1e-3` with a simple pointwise forward expansion feature extractor. 
+Default benchmark:
+```python
+python ex1_burgers.py
+```
+No subsampling (8192 grid), Galerkin-type attention, adding a diagonal matrix to the Xavier initialization of the `W^Q, W^K, W^V` matrices.
+```python
+python ex1_burgers.py --subsample 1 --attn-type 'galerkin' --xavier-init 0.01 --diag-weight 0.01
+```
 
 ## Interface Darcy flow
 
@@ -14,8 +28,8 @@ The data is of courtesy of [Mr. Zongyi Li (Caltech)](https://github.com/zongyi-l
 
 
 # License
-This software is distributed with the **MIT** license which translates roughly that you can use it however you want and for whatever reason you want. All the
+This software is distributed with the MIT license which translates roughly that you can use it however you want and for whatever reason you want. All the
 information regarding support, copyright and the license can be found in the [LICENSE](./LICENSE) file.
 
 # Acknowledgement
-The hardware to perform this work is provided by Andromeda Saving Fund. This work was supported in part by the National Science Foundation under grants DMS-1913080. We would like to thank [Dr. Long Chen (UC Irvine)](github.com/lyc102) for the inspiration and encouragement on the initial conceiving of this paper. We would like to thank Dr. Ruchi Guo (UC Irvine) and Dr. Yuanzhe Xi (Emory) for some early feedback on the choice of the numerical experiements. We would like to thank [Mr. Zongyi Li (Caltech)](https://github.com/zongyi-li) for sharing some early dev code in the updated PyTorch fft interface.  We would like to thank Joel Schlosser(Facebook) to incorporate our change to the PyTorch transformer module to simplify our testing pipeline.  We would be grateful to the PyTorch community for selflessly code sharing, including Phil Wang([lucidrains@github](https://github.com/lucidrains)) and [Harvard NLP group Klein et al. (2017)](https://nlp.seas.harvard.edu/2018/04/03/attention.html).  Wewould like to thank the chebfun Driscoll et al. (2014) for integrating powerful tools into a simple interface to solve PDEs. We would like to thank Yannic Kilcher (ykilcher@twitter)for frequently covering the newest research on Transformers in video formats.  We wouldalso like to thank the Python community (Van Rossum and Drake (2009); Oliphant (2007))for sharing and developing the tools that enabled this work, including Pytorch Paszke et al.(2017),  NumPy Harris et al. (2020),  SciPy Virtanen et al. (2020),  Plotly Inc. (2015) andMatplotlib Hunter (2007).
+The hardware to perform this work is provided by Andromeda Saving Fund. This work was supported in part by the National Science Foundation under grants DMS-1913080. We would like to thank [Dr. Long Chen (Univ of California Irvine)](github.com/lyc102) for the inspiration and encouragement on the initial conceiving of this paper. We would like to thank Dr. Ruchi Guo (Univ of California Irvine) and Dr. Yuanzhe Xi (Emory) for some early feedback on the choice of the numerical experiements. We would like to thank [Mr. Zongyi Li (Caltech)](https://github.com/zongyi-li) for sharing some early dev code in the updated PyTorch `torch.fft` interface.  We would like to thank [Joel Schlosser](https://github.com/jbschlosser) to incorporate our change to the PyTorch `transformer` submodule to simplify our testing pipeline.  We would be grateful to the PyTorch community for selflessly code sharing, including Phil Wang([lucidrains@github](https://github.com/lucidrains)) and [Harvard NLP group Klein et al. (2017)](https://nlp.seas.harvard.edu/2018/04/03/attention.html).  Wewould like to thank the chebfun Driscoll et al. (2014) for integrating powerful tools into a simple interface to solve PDEs. We would like to thank [Yannic Kilcher](https://www.youtube.com/c/YannicKilcher/about) and [Hung-yi Lee (National Taiwan University)](https://www.youtube.com/c/HungyiLeeNTU) for frequently covering the newest research on Transformers in video formats.  We would also like to thank the Python community (Van Rossum and Drake (2009); Oliphant (2007))for sharing and developing the tools that enabled this work, including Pytorch Paszke et al.(2017),  NumPy Harris et al. (2020),  SciPy Virtanen et al. (2020),  Plotly Inc. (2015) andMatplotlib Hunter (2007).
--- ex1_burgers.py
+++ ex1_burgers.py
@@ -1,54 +1,96 @@
 from libs import *
-
+import argparse
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import OneCycleLR
 
 SEED = 1127802
-get_seed(SEED)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 DEBUG = False
 
 subsample = 4
 batch_size = 8
+val_batch_size = 4
 
-def get_data():
+def get_data(train_portion=1024, 
+             valid_portion=100, 
+             random_state=SEED,
+             batch_size=batch_size,
+             val_batch_size=val_batch_size,
+             subsample=subsample,
+             **kwargs):
 
     data_path = os.path.join(DATA_PATH, 'burgers_data_R10.mat')
     train_dataset = BurgersDataset(subsample=subsample,
                                    train_data=True,
-                                   train_portion=0.5,
+                                   train_portion=train_portion,
                                    data_path=data_path,
-                                   random_state=SEED)
+                                   random_state=random_state)
 
     valid_dataset = BurgersDataset(subsample=subsample,
                                    train_data=False,
-                                   valid_portion=100,
+                                   valid_portion=valid_portion,
                                    data_path=data_path,
-                                   random_state=SEED)
+                                   random_state=random_state)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True,
-                              pin_memory=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=4, shuffle=False, drop_last=False,
-                              pin_memory=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, 
+                              drop_last=True, **kwargs)
+    valid_loader = DataLoader(valid_dataset, batch_size=val_batch_size, shuffle=False, 
+                              drop_last=False,**kwargs)
     return train_loader, valid_loader
 
 
 def main():
-    train_loader, valid_loader = get_data()
+
+    # Training settings
+    parser = argparse.ArgumentParser(description='Example 1: Burgers equation')
+    parser.add_argument('--subsample', type=int, default=4, metavar='subsample',
+                        help='input sampling from 8192 (default: 4 i.e., 2048 grid)')
+    parser.add_argument('--batch-size', type=int, default=8, metavar='n_b',
+                        help='input batch size for training (default: 8)')
+    parser.add_argument('--val-batch-size', type=int, default=4, metavar='n_b',
+                        help='input batch size for validation (default: 4)')
+    parser.add_argument('--attn-type', type=str, default='fourier', metavar='attn_type',
+                        help='input attention type for encoders (possile: fourier (alias integral, local), galerkin (alias global), softmax (official PyTorch implementation), linear (standard Q(K^TV) with softmax), default: fourier)')
+    parser.add_argument('--xavier-init', type=float, default=1e-2, metavar='xavier_init',
+                        help='input Xavier initialization strength for Q,K,V weights (default: 0.01)')
+    parser.add_argument('--diag-weight', type=float, default=1e-2, metavar='diag_weight',
+                        help='input diagonal weight initialization strength for Q,K,V weights (default: 0.01)')
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
+                        help='max learning rate (default: 0.001)')
+    parser.add_argument('--gamma', type=float, default=1e-1, metavar='regularizer',
+                        help='strength of gradient regularizer (default: 0.1)')
+    parser.add_argument('--cuda', action='store_true', default=True,
+                        help='enables GPU training')
+    parser.add_argument('--seed', type=int, default=SEED, metavar='Seed',
+                        help='random seed (default: 1127802)')
+    args = parser.parse_args()
+    cuda = args.cuda and torch.cuda.is_available()
+    device = torch.device('cuda' if cuda else 'cpu')
+    kwargs = {'pin_memory': True} if cuda else {}
+
+    get_seed(args.seed)
+
+    train_loader, valid_loader = get_data(subsample=args.subsample,
+                                          batch_size=args.batch_size,
+                                          val_batch_size=args.val_batch_size,
+                                          **kwargs)
     sample = next(iter(train_loader))
+
+    print('='*20, 'Data loader batch', '='*20)
     for key in sample.keys():
         print(key, "\t", sample[key].shape)
-
-    f = sample['node']
-    pos = sample['pos']
-    u = sample['target']
+    print('='*(40 + len('Data loader batch')+2))
 
     if is_interactive():
+        u0 = sample['node']
+        pos = sample['pos']
+        u = sample['target']
         _, axes = plt.subplots(nrows=4, ncols=1, figsize=(15, 10))
         axes = axes.reshape(-1)
         indexes = np.random.choice(range(4), size=4, replace=False)
         for i, ix in enumerate(indexes):
-            axes[i].plot(pos[ix], f[ix], label='input')
+            axes[i].plot(pos[ix], u0[ix], label='input')
             axes[i].plot(pos[ix], u[ix, :, 0], label='target')
             axes[i].plot(pos[ix, 1:-1], u[ix, 1:-1, 1],
                          label='target derivative')
@@ -58,20 +100,23 @@ def main():
         config = yaml.full_load(f)
     test_name = os.path.basename(__file__).split('.')[0]
     config = config[test_name]
+    config['attention_type'] = args.attn_type
+    config['diagonal_weight'] = args.diag_weight
+    config['xavier_init'] = args.xavier_init
 
     torch.cuda.empty_cache()
     model = FourierTransformer(**config)
     model = model.to(device)
     print(f"\nNumber of params: {get_num_params(model)}")
 
-    epochs = 100
-    lr = 1e-3
+    epochs = args.epochs
+    lr = args.lr
     h = (1/2**13)*subsample
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     scheduler = OneCycleLR(optimizer, max_lr=lr, div_factor=1e4, final_div_factor=1e4,
                            steps_per_epoch=len(train_loader), epochs=epochs)
 
-    loss_func = WeightedL2Loss(regularizer=True, h=h, gamma=0.1)
+    loss_func = WeightedL2Loss(regularizer=True, h=h, gamma=args.gamma)
 
     metric_func = WeightedL2Loss(regularizer=False, h=h)
 
